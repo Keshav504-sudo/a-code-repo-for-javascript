c4bc60fc7dc4005b9762542d3f3633e2e5f8941c
keshav dixit <dixit.keshav04@gmail.com>
2026-07-01 07:57:18 +0000

    datatypes

---
+++ basics/01_variables.js
@@ -0,0 +1,12 @@
+const account_id = 9945//can't change
+let email_id = "abc@gmail.com"//can change
+var pass = "12345"
+account_city = "jaipur"
+let account_state;
+
+pass = "123765"
+email_id = "hc@ggmail.com"
+console.log(pass)
+console.log(email_id)
+console.log(account_city)
+console.table([account_id,pass, email_id, account_city, account_state])//shows in tabular format
