411c5ce78c7f97ba789223c9e5bf98094e77ebee
keshav dixit <dixit.keshav04@gmail.com>
2026-07-22 13:20:57 +0000

    loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,19 @@
+let index = 3
+while (index <= 10){
+   //  console.log(`val of index is ${index}`);
+    index = index + 2
+}
+
+let myarray = ['flash','hulk','thor']
+let arr = 0
+while (arr < myarray.length){
+    //console.log(`vaalue is ${myarray[arr]}`);
+    arr = arr + 1
+}
+
+// do while first print the value of score then increment it by 100 and check if it is less than or equal to 400
+ let score = 100
+ do {
+    console.log(`the score is ${score}`);
+    score +=100}
+while (score <=400)
